e2334138fb5ddd0649aac8c71fd213d7cb791953
asaf <asaf@flexi-core.com>
2018-09-17 01:40:58 +0300

    2.0.2 - removed all operators handlers etc , this is a mid service dependecy , method execution was trasferred to FC itself , this service and its model should only define the objects that could be saved into the DB / executed by invokers , the actual invocation will be handled in the scheduling service / FC REST , import CSV

--- src/main/java/com/flexicore/product/config/Config.java
+++ src/main/java/com/flexicore/product/config/Config.java
@@ -2,21 +2,21 @@ package com.flexicore.product.config;
 
 import com.flexicore.annotations.InjectProperties;
 import com.flexicore.annotations.plugins.PluginInfo;
+import com.flexicore.data.jsoncontainers.CrossLoaderResolver;
 import com.flexicore.interfaces.InitPlugin;
+import com.flexicore.product.model.InspectEquipmentRequest;
 
 import javax.inject.Inject;
 import java.util.Properties;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-@PluginInfo(version = 1,autoInstansiate = true)
+@PluginInfo(version = 1, autoInstansiate = true)
 public class Config implements InitPlugin {
 
-    private static AtomicBoolean init=new AtomicBoolean(false);
-    private static int SYNC_MAX_THREADS=1;
-    private static int SYNC_MAX_THREADS_FOR_HANDLERS=5;
-    private static long SYNCER_CHECK_INTERVAL=1000*60;//every 1 min
-
-
+    private static AtomicBoolean init = new AtomicBoolean(false);
+    private static int SYNC_MAX_THREADS = 1;
+    private static int SYNC_MAX_THREADS_FOR_HANDLERS = 5;
+    private static long SYNCER_CHECK_INTERVAL = 1000 * 60;//every 1 min
 
 
     @Inject
@@ -25,18 +25,17 @@ public class Config implements InitPlugin {
 
     @Override
     public void init() {
-        if(init.compareAndSet(false,true)){
-            SYNCER_CHECK_INTERVAL=Long.parseLong(properties.getProperty("SYNCER_CHECK_INTERVAL",SYNCER_CHECK_INTERVAL+""));
+        if (init.compareAndSet(false, true)) {
+            SYNCER_CHECK_INTERVAL = Long.parseLong(properties.getProperty("SYNCER_CHECK_INTERVAL", SYNCER_CHECK_INTERVAL + ""));
 
-            SYNC_MAX_THREADS=Integer.parseInt(properties.getProperty("SYNC_MAX_THREADS",SYNC_MAX_THREADS+""));
-            SYNC_MAX_THREADS_FOR_HANDLERS=Integer.parseInt(properties.getProperty("SYNC_MAX_THREADS_FOR_HANDLERS",SYNC_MAX_THREADS_FOR_HANDLERS+""));
+            SYNC_MAX_THREADS = Integer.parseInt(properties.getProperty("SYNC_MAX_THREADS", SYNC_MAX_THREADS + ""));
+            SYNC_MAX_THREADS_FOR_HANDLERS = Integer.parseInt(properties.getProperty("SYNC_MAX_THREADS_FOR_HANDLERS", SYNC_MAX_THREADS_FOR_HANDLERS + ""));
 
-
+            CrossLoaderResolver.registerClass(InspectEquipmentRequest.class);
 
 
         }
     }
-
 
 
     public static int getSyncMaxThreads() {
