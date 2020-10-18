abc7821d6b85170a5b1d5a731f2519a5a68a37ef
asaf <asaf@flexi-core.com>
2020-10-18 15:35:46 +0300

    3.3.3 - fc 4.2.0 compatibility

---
+++ src/main/java/com/flexicore/product/config/TransactionSupportSupport.java
@@ -0,0 +1,14 @@
+package com.flexicore.product.config;
+
+import com.flexicore.interfaces.ServicePlugin;
+import org.pf4j.Extension;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@Extension
+@EnableTransactionManagement(proxyTargetClass = true)
+public class TransactionSupportSupport implements ServicePlugin {
+
+}
